b32c3149ea41fda502172ecff87b1be7f5bfdaa3
Hendrik Norkowski <hnorkowski@mail.de>
2025-07-05 13:55:47 +0200

    feat: add MessageId enum example

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,32 @@ impl User {
     }
 }
 
+#[derive(MessageId)]
+#[message_id = 0x13]
+#[allow(dead_code)]
+enum UnsignedInteger {
+    U32(u32),
+    U64(u64),
+    U128(u128),
+}
+
+impl UnsignedInteger {
+    /// Encodes an unsigned integer into big endian bytes and prepends the length of the number in bytes as single u8
+    fn encode_content(&self) -> Vec<u8> {
+        let mut encoded = match self {
+            Self::U32(number) => number.to_be_bytes().to_vec(),
+            Self::U64(number) => number.to_be_bytes().to_vec(),
+            Self::U128(number) => number.to_be_bytes().to_vec(),
+        };
+
+        let size =
+            u8::try_from(encoded.len()).expect("Maximum of 16 bytes => should always fit in an u8");
+        encoded.insert(0, size);
+
+        encoded
+    }
+}
+
 fn main() {
     let user = User {
         name: "John Doe".to_string(),
@@ -19,4 +45,9 @@ fn main() {
 
     dbg!(User::MESSAGE_ID);
     dbg!(user.encode());
+
+    let tlv = UnsignedInteger::U64(0x1337);
+
+    dbg!(UnsignedInteger::MESSAGE_ID);
+    dbg!(tlv.encode());
 }
